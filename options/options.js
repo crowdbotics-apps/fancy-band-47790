c884f93ad94100808723eb63b40956418999bec1
Crowdbotics <team@crowdbotics.com>
2024-05-02 19:40:13 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-band-47790",
+  url: "https://fancy-band-47790.botics.co",
+  api: "https://fancy-band-47790.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
